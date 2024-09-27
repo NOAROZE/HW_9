9a3fd952a35ac0686e4890f131c16d5d81a1f007
NOAROZE <156937034+NOAROZE@users.noreply.github.com>
2024-09-27 13:33:43 +0300

    Update num_3.py

--- num_3.py
+++ num_3.py
@@ -1,6 +1,6 @@
 numbers: list[int] = []
 SENTINEL: int = -999
-lculation: int = 0
+calculation: int = 0
 new_numbers: list[int] = []
 while True:
     num: int = int(input("enter a number between 1-10:"))
@@ -12,9 +12,9 @@ while True:
     for i in range(0, 10):
         if i in numbers:
             print("statistic",[i]," = ",numbers.count(i))
-while lculation < 10:
+while calculation < 10:
     new_numbers.append(numbers.count(j))
-    lculation += 1
+    calculation += 1
 print(new_numbers)
 # האם תוכל לפתור עבור טווח של 0-100?
 # כן אבל אם הקלטים היו גם בין 0-100 גם עכשיו היה אפשר אבל מ -10 הכל היה ריק
